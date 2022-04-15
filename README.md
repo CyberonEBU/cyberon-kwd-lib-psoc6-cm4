ef05e414e288c05ed5eee17cd7d888ebe8abb580
knave_lin <knave_lin@cyberon.com.tw>
2022-04-15 10:56:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PSoC 6 MCU: Cyberon Keyword Detection Engine Lib
+
+Check out what's new for PSoC 6 about voice recognition.
+
+Go to
+- [Cyberon Keyword Detection Engine Demo - One-stage](https://github.com/CyberonEBU/cyberon-one-stage-kwd-demo-psoc6-cm4)
+- [Cyberon Keyword Detection Engine Demo - Two-stage](https://github.com/CyberonEBU/cyberon-two-stage-kwd-demo-psoc6-cm4)
---
+++ include/base_types.h
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2022 Cyberon Corporation
+ *
+ * you may not use this file except in compliance with the License.
+ * A copy of the license is located in the "LICENSE" file accompanying this source.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#ifndef	__BASE_TYPES_H
+#define	__BASE_TYPES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define _CEVA_SIMULATE_
+
+#if defined(_WIN32)
+
+
+#ifdef _CEVA_SIMULATE_
+	typedef signed short CHAR;
+	typedef unsigned short BYTE;
+	typedef signed short INT8;
+	typedef unsigned short UINT8;
+	typedef signed short SHORT;
+	typedef unsigned short WORD;
+	typedef unsigned short USHORT;
+	typedef signed long LONG;
+	typedef unsigned int DWORD;
+	typedef unsigned long ULONG;
+	typedef signed int INT;
+	typedef unsigned int UINT;
+	typedef int BOOL;
+	typedef void VOID;
+	typedef unsigned short WCHAR;	/*typedef wchar_t WCHAR;*/
+	typedef float FLOAT;
+#else
+	#include <windows.h>
+	typedef signed char INT8;
+	typedef unsigned char UINT8;
+	typedef unsigned short USHORT;
+	typedef unsigned int UINT;
+	typedef unsigned long ULONG;
+#endif
+
+	
+#if (_MSC_VER < 1400)
+	typedef unsigned short WCHAR;
+#endif
+
+#else
+
+#ifdef _CEVA_SIMULATE_
+	typedef signed short CHAR;
+	typedef unsigned short BYTE;
+	typedef signed short INT8;
+	typedef unsigned short UINT8;
+#else
+	typedef signed char CHAR;
+	typedef unsigned char BYTE;
+	typedef signed char INT8;
+	typedef unsigned char UINT8;
+#endif
+
+	typedef signed short SHORT;
+	typedef unsigned short WORD;
+	typedef unsigned short USHORT;
+	typedef signed long LONG;
+	typedef unsigned int DWORD;
+	typedef unsigned long ULONG;
+	typedef signed int INT;
+	typedef unsigned int UINT;
+	typedef int BOOL;
+	typedef void VOID;
+	typedef unsigned short WCHAR;	/*typedef wchar_t WCHAR;*/
+	typedef float FLOAT;
+
+#endif
+
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
+#define UNICODE WCHAR
+
+#ifdef LPVOID
+#undef LPVOID
+#endif
+#define LPVOID	void*
+
+#ifndef TRUE
+#define TRUE	(1 == 1)
+#endif
+
+#ifndef FALSE
+#define FALSE	(1 == 0)
+#endif
+
+#ifndef HANDLE
+#define HANDLE	VOID*
+#endif
+
+#ifndef NULL
+#define NULL	((VOID*)0)
+#endif
+
+#define PNULL	((VOID*)0)
+
+#ifndef STATIC
+#define STATIC 	static
+#endif
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+#if defined(_WIN32)
+	#define EXPAPI WINAPI
+#else
+	#define EXPAPI
+#endif
+
+#ifndef SAFE_DELETE
+#define SAFE_DELETE(a)	if ( (a) != NULL ) {delete (a); (a) = NULL;}
+#endif
+
+#ifndef SAFE_FREE
+#define SAFE_FREE(a)	if ( (a) != NULL ) {free(a); (a) = NULL;}
+#endif
+
+#if !defined(_WIN32)
+//#include "SubName.h"
+//#include "License_SubName.h"
+#endif
+
+//#include "DMemory.h"
+
+#ifndef INLINE
+#if defined ( _WIN32 )
+	#define INLINE		__inline		// inline keyword for WIN32 Compiler
+#elif defined ( __CC_ARM )
+	#define INLINE		__inline		// inline keyword for ARM Compiler
+#elif defined ( __ICCARM__ )
+	#define INLINE		inline			// inline keyword for IAR Compiler. Only available in High optimization mode!
+#elif defined ( __GNUC__ )
+	#define INLINE		inline			// inline keyword for GNU Compiler
+#else
+	#define INLINE		inline
+#endif
+#endif	// INLINE
+
+#endif	/* __BASE_TYPES_H */
